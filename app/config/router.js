29c465818c56038c54cc679929af30fe26d4d3e2
Michael Marcinek <michaelmarcinek@gmail.com>
2017-11-17 10:50:11 -0500

    changes router titles

--- app/config/router.js
+++ app/config/router.js
@@ -5,6 +5,6 @@ import JobList from '../containers/JobList';
 import JobTable from '../containers/JobTable';
   
   export const Nav = StackNavigator({
-    JobList: { screen: JobList, navigationOptions: {title: 'Jobs Mother Fucker!'} },
-    JobTable: { screen: JobTable, navigationOptions: { title: 'Job Detail'} },
+    JobList: { screen: JobList, navigationOptions: {title: 'Jobs'} },
+    JobTable: { screen: JobTable, navigationOptions: { title: 'Sample Data'} },
   });
